398900761b1d1096a06c64f5d29ce29ec351710d
Shihab <iamshihabs@gmail.com>
2023-08-18 16:42:02 +0530

    small change

--- client/src/components/admin/headerWithSide.tsx
+++ client/src/components/admin/headerWithSide.tsx
@@ -3,6 +3,7 @@
 
 const HeaderWithSidebar  = () => {
   return (
+    
     <header>
        <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
