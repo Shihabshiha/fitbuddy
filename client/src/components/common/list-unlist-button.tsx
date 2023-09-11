7bd8f32ba1432b313be39123017e4efe7c7546b2
Shihab <iamshihabs@gmail.com>
2023-09-11 15:20:24 +0530

    List and Unlist of course done

---
+++ client/src/components/common/list-unlist-button.tsx
@@ -0,0 +1,50 @@
+// ListUnlistButton.tsx
+import { BeatLoader } from 'react-spinners';
+import React, { useState } from 'react';
+import { listUnlist } from '../../api/endpoints/trainer';
+import { Course } from '../../types/courseType';
+
+interface ListUnlistButtonProps {
+  course: Course;
+  onUpdateCourse: (updatedCourse: Course) => void;
+}
+
+const ListUnlistButton: React.FC<ListUnlistButtonProps> = ({ course, onUpdateCourse }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleButtonClick = async () => {
+    try {
+      setLoading(true);
+      // const updatedCourse = await axios.post(`/api/update-list-status/${course._id}`, {
+      //   isListed: !course.isListed,
+      // });
+      const updatedCourse = await listUnlist(course._id, !course.isListed)
+
+      // Call the onUpdateCourse callback to update the course in the parent component
+      onUpdateCourse(updatedCourse.data);
+
+      setLoading(false);
+    } catch (error) {
+      // Handle API call errors
+      setLoading(false);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleButtonClick}
+      className={`${
+        loading
+          ? 'bg-gray-400 cursor-not-allowed'
+          : course.isListed
+          ? 'bg-red-500 hover:bg-red-600'
+          : 'bg-green-500 hover:bg-green-600'
+      } text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-300`}
+      disabled={loading}
+    >
+      {loading ? <BeatLoader size={8} color="white" /> : course.isListed ? 'Unlist' : 'List'}
+    </button>
+  );
+};
+
+export default ListUnlistButton;
--- client/src/components/pages/trainer-pages/my-courses.tsx
+++ client/src/components/pages/trainer-pages/my-courses.tsx
@@ -1,110 +1,132 @@
 import React , { useEffect, useState  } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom'; 
 import { notify , ToastContainer } from '../../../utils/notificationUtils';
-import { ScaleLoader } from 'react-spinners';
+import { AxiosError } from 'axios';
+import { getAllCourses } from '../../../api/endpoints/trainer';
+import { Course } from '../../../types/courseType';
+import CourseTableShimmer from '../../shimmers/course-table-shimmer';
+import ListUnlistButton from '../../common/list-unlist-button';
 
-interface Course {
-  id: number;
-  courseName: string;
-  createdDate: string;
-  subscribers: number;
-  chapters: number;
-  editLink: string;
-  thumbnail: string;
-}
+
 
 const CoursesTable: React.FC = () => {
   const navigate = useNavigate();
   const [loading,setLoading] = useState(false)
-
-  const courses: Course[] = [
-    {
-      id: 1,
-      courseName: 'Course 1',
-      createdDate: '2023-09-01',
-      subscribers: 50,
-      chapters: 10,
-      editLink: '/edit-course/1', // Replace with the actual edit link
-      thumbnail: 'course1.jpg', // Replace with the actual thumbnail URL
-    },
-    // Add more courses as needed
-  ];
+  const [courses,setCourses] = useState<Course[]>([])
 
   useEffect(()=>{
-    
-  })
+    setLoading(true)
+    const fetchCourses =async () => {
+      try{
+        const response = await getAllCourses()
+        const coursesData = response?.data?.result;
+        console.log('courses',coursesData)
+        setCourses(coursesData || [])
+      }catch(error:unknown){
+        if (error instanceof AxiosError && error.response?.data?.error) {
+          notify(error.response.data.error, "error");
+        } else {
+          notify("An error occured in fetching trainers data.", "error");
+        }
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchCourses()
+  },[])
 
 
+  const handleUpdatedCourse = (updatedCourse: Course) => {
+    setCourses((prevCourses) =>
+      prevCourses.map((c) =>
+        c._id === updatedCourse._id ? updatedCourse : c
+      )
+    );
+  };
 
+
+  const formatDate = (dateString: string): string => {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+  
 
   return (
     <div>
       {loading && (
-        <div className="fixed inset-0 flex justify-center items-center bg-opacity-50 bg-white">
-          <ScaleLoader color="#007BFF" loading={true} />
-        </div>
+        <>
+          <CourseTableShimmer />
+        </>
       )}
-      <div className="flex justify-end mb-4">
-      <button
-        onClick={()=>{
-          navigate('/trainer/add-course')
-        }}
-        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
-      >
-        Add Course
-      </button>
 
-      </div>
-      {courses.length === 0 ? (
-        <div className="bg-white border rounded-lg p-8 text-center">
-          <p className="text-gray-500 text-lg">No courses added.</p>
-          <p className="text-gray-500 text-lg">Click "Add Course" to create a new course.</p>
+      {!loading && (
+        <div>
+          <div className="flex justify-end mb-4">
+            <button
+              onClick={()=>{
+                navigate('/trainer/add-course')
+              }}
+              className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+            >
+              Add Course
+            </button>
+          </div>
+
+          {courses.length === 0 ? (
+            <div className="bg-white border rounded-lg p-8 text-center">
+              <p className="text-gray-500 text-lg">No courses added.</p>
+              <p className="text-gray-500 text-lg">Click "Add Course" to create a new course.</p>
+            </div>
+          ) : (
+            <table className="min-w-full border divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Thumbnail
+                  </th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Course Name
+                  </th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Created Date
+                  </th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    List / Unlist
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {courses.map((course) => (
+                  <tr key={course._id}>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      <img src={course.thumbnailUrl} alt={course.courseName} className="h-8 w-8" />
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap ">{course.courseName}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap ">{formatDate(course.createdAt)}</td>
+                    <td className="px-6 py-4 whitespace-no-wrap">
+                      {/* <button
+                        onClick={() => handleListUnlist(course)}
+                        className={`${
+                          course.isListed ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600'
+                        } text-white px-4 py-2 rounded-md focus:outline-none focus:ring focus:ring-blue-300`}
+                      >
+                        {course.isListed ? "Unlist" : "List"}
+                      </button> */}
+                      <ListUnlistButton course={course} onUpdateCourse={handleUpdatedCourse} />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
-      ) : (
-        <table className="min-w-full border divide-y divide-gray-200">
-          <thead>
-            <tr>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                Thumbnail
-              </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                Course Name
-              </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                Created Date
-              </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                Subscribers
-              </th>
-              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                Chapters/Videos
-              </th>
-              <th className="px-6 py-3 bg-gray-50"></th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {courses.map((course) => (
-              <tr key={course.id}>
-                <td className="px-6 py-4 whitespace-no-wrap">
-                  <img src={course.thumbnail} alt={course.courseName} className="h-8 w-8" />
-                </td>
-                <td className="px-6 py-4 whitespace-no-wrap ">{course.courseName}</td>
-                <td className="px-6 py-4 whitespace-no-wrap ">{course.createdDate}</td>
-                <td className="px-6 py-4 whitespace-no-wrap ">{course.subscribers}</td>
-                <td className="px-6 py-4 whitespace-no-wrap ">{course.chapters}</td>
-                <td className="px-6 py-4 whitespace-no-wrap ">
-                  <Link to={course.editLink} className="text-blue-600 hover:underline">
-                    View
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       )}
+
       <ToastContainer />
-    </div>
+      </div>
   );
+  
 };
 
 export default CoursesTable;
+
