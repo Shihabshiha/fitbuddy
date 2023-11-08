8faf373771ee4304cfa010fa6326ba076c717148
Shihab <iamshihabs@gmail.com>
2023-11-08 20:03:59 +0530

    http changed to https

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ import bodyParser from 'body-parser';
 const app = express()
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(routes)
